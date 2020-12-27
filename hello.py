9d96dce220d0b12b23d6cf63dce8ae040c2b2f01
Harry Potter <harry.potter@example.com>
2020-12-27 15:46:31 +0200

    feature update to hello function

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+def hello(name,age):
+    if age >= 18:
+        print("Hello %s, you are an adult!" % (name))
+    else:
+        print("Hello %s, you are a kid!" % (name))
+
+name=str(input("What is your name?: "))
+age=int(input("How old are you?: "))
+
+hello(name,age)
