5c33c20edd50e82e380d96745dc822fdae94f304
Wilhelm de Lange <wilhelmdl21@gmail.com>
2023-02-02 15:22:28 +0400

    test commit

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 def hello(name,age):
-    if age >= 18:
+    if age < 18:
+        print("Hello %s, you are a kid!" % (name))
+    elif age < 50:
         print("Hello %s, you are an adult!" % (name))
     else:
-        print("Hello %s, you are a kid!" % (name))
+        print("Hello %s, you are ooooold!" % (name))
 
 name=str(input("What is your name?: "))
 age=int(input("How old are you?: "))
